f997584beb9acebe13315c8ed5e04707d4d1bdd7
thuanluong31241027504-sketch <thuanluong.31241027504@st.ueh.edu.vn>
2026-06-04 01:16:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
+import onnxruntime as ort
 import io
 import os
 
@@ -77,28 +78,25 @@ st.markdown("""
 
 st.markdown('<div class="main-title">vietnamese money recognition<span class="blinking-cursor">_</span></div>', unsafe_allow_html=True)
 
-# Fake model - chỉ để demo
-if 'predict_count' not in st.session_state:
-    st.session_state.predict_count = 0
+MODEL_FILE = "vietnamese_money.onnx"
 
-class FakeSession:
-    def get_inputs(self):
-        return [type('obj', (object,), {'name': 'input', 'shape': [1, 128, 128, 3]})()]
-    
-    def run(self, output_names, input_feed):
-        st.session_state.predict_count += 1
-        # Lần 1,3,5,... ra 50k
-        if st.session_state.predict_count % 2 == 1:
-            # 50.000 dong
-            return [np.array([[0.01, 0.01, 0.92, 0.03, 0.02, 0.01]], dtype=np.float32)]
-        else:
-            # 10.000 dong
-            return [np.array([[0.92, 0.02, 0.01, 0.02, 0.02, 0.01]], dtype=np.float32)]
+@st.cache_resource
+def load_model():
+    if os.path.exists(MODEL_FILE):
+        return ort.InferenceSession(MODEL_FILE)
+    return None
 
-session = FakeSession()
+session = load_model()
+
+if session is None:
+    st.error("vietnamese_money.onnx not found")
+    st.stop()
+
 input_info = session.get_inputs()[0]
-target_size = (128, 128)
+input_shape = input_info.shape
+target_size = (input_shape[1], input_shape[2])
 
+CLASS_NAMES = ['010000', '020000', '050000', '100000', '200000', '500000']
 DISPLAY_NAMES = ['10.000 dong', '20.000 dong', '50.000 dong', '100.000 dong', '200.000 dong', '500.000 dong']
 
 MONEY_INFO = {
@@ -134,6 +132,14 @@ MONEY_INFO = {
     }
 }
 
+def preprocess_image(img):
+    if img.mode == 'RGBA':
+        img = img.convert('RGB')
+    img = img.resize(target_size)
+    img_array = np.array(img).astype(np.float32) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
 col_left, col_right = st.columns([0.5, 0.5])
 
 with col_left:
@@ -143,10 +149,12 @@ with col_left:
     if camera_image is not None:
         bytes_data = camera_image.getvalue()
         img = Image.open(io.BytesIO(bytes_data))
-        st.image(img, width=250)
+        st.image(img, width=250, caption="anh da chup")
         
         if st.button("predict"):
-            predictions = session.run(None, {input_info.name: None})[0][0]
+            img_array = preprocess_image(img)
+            input_name = input_info.name
+            predictions = session.run(None, {input_name: img_array})[0][0]
             
             st.markdown("---")
             st.markdown("xac suat tung menh gia")
@@ -185,4 +193,4 @@ with col_right:
             """, unsafe_allow_html=True)
 
 st.markdown("---")
-st.caption("version 1.0 | vietnam money recognition")
+st.caption("version 1.0 | vietnam money recognition cnn")
